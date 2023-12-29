9eb765e36a146ff9e0a9e8333590637b8a4e7db1
alisonjoe <mr.lai.who@gmail.com>
2023-12-29 08:17:46 +0800

    feat(web tempates): tempates

--- streamonitor/managers/httpmanager.py
+++ streamonitor/managers/httpmanager.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import os
 from streamonitor.bot import Bot
 import streamonitor.log as log
 from streamonitor.manager import Manager
 
+app = Flask(__name__)
 
 class HTTPManager(Manager):
     def __init__(self, streamers):
@@ -11,53 +12,21 @@ class HTTPManager(Manager):
         self.logger = log.Logger("manager")
 
     def run(self):
-        app = Flask(__name__)
-
-        def header():
-            return "<h1>CG-DL Status</h1>"
-
-        def scripts():
-            pass
-
         @app.route('/')
         def status():
-            output = header()
-            output += """
-                <table style="border:1px solid">
-                <tr>
-                <th>Site</th>
-                <th>Username</th>
-                <th>Started</th>
-                <th>Status</th>
-                </tr>"""
-
-            for streamer in self.streamers:
-                output += """
-                    <tr>
-                    <td>{s}</td>
-                    <td><a href="/recordings?user={u}&site={s}">{u}</a></td>
-                    <td><a onclick="togglerunning({u})">{r}</a></td>
-                    <td><a onclick="refreshstatus({u})">{st}</a></td>
-                    </tr>""".format(s=streamer.site, r=streamer.running,
-                                    st=streamer.status(), u=streamer.username)
-            output += "</table>"
-            return output
+            return render_template('status.html', streamers=self.streamers)
 
         @app.route('/recordings')
         def recordings():
-            output = header()
-            streamer = self.getStreamer(request.args.get("user"), request.args.get("site"))
-            output += "<p>Recordings of {u} [{s}]</p>".format(u=streamer.username, s=streamer.siteslug)
+            user = request.args.get("user")
+            site = request.args.get("site")
+            streamer = self.getStreamer(user, site)
+            recordings_list = []
             try:
-                temp = "<p>"
-                for elem in os.listdir("./downloads/{u} [{s}]".format(u=streamer.username, s=streamer.siteslug)):
-                    temp += elem + "<br>"
-                if temp == "<p>":
-                    output = "<p>No recordings</p>"
-                else:
-                    output += temp + "</p>"
-            except:
-                output += "<p>No recordings</p>"
-            return output
+                recordings_list = os.listdir(f"./downloads/{user} [{site}]")
+            except FileNotFoundError:
+                pass
+
+            return render_template('recordings.html', streamer=streamer, recordings=recordings_list)
 
         app.run(host='127.0.0.1', port=5000)
